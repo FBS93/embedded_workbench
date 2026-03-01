8715e359759ca36fa383f8ce44a57ba931999558
FBS <fbs.ioctl@gmail.com>
2026-03-01 14:50:04 +0000

    Move compile flags from toolchain to presets
    (cherry picked from commit 5a83cac4bdca76f1561bd8617df1f3093003e9db)

--- tools/cmake/toolchains/arm_gcc.cmake
+++ tools/cmake/toolchains/arm_gcc.cmake
@@ -6,9 +6,6 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 
-set(CMAKE_C_FLAGS_DEBUG "-O0 -g3")
-set(CMAKE_C_FLAGS_RELEASE "-Os -g2")
-
 add_compile_options(
     -mthumb
     -ffunction-sections
